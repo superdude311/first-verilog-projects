ee51897272bd0733ea2c59902e64db8cf3db42bd
Matthew K <97872597+superdude311@users.noreply.github.com>
2025-10-24 21:51:36 -0400

    fixed syntax + formatting

--- 6502multaccel.v
+++ 6502multaccel.v
@@ -5,29 +5,31 @@ module multaccel (inout wire [7:0]D,
                 input logic RWB, 
                 input logic CE, 
                 input logic A0, 
-                input logic clk, 
-                output logic [7:0]A,
-                output logic [7:0]B,
-                output logic [15:0]out,
-                output logic [7:0]Dout)
+                input logic clk)
+
+    logic [7:0]A;
+    logic [7:0]B;
+    logic [15:0]out;
+    logic [7:0]Dout;
 
     // input when CE high and RWB 1 (reading)
     always_ff @(posedge clk) begin
         if (CE && RWB && A0) begin
             // input to B register if A0 is 1
-            B <= D // FYI this symbol is non-blocking assignment
+            B <= D; // FYI this symbol is non-blocking assignment
         end else if (CE && RWB && !A0) begin
             // input to A register if A0 is 0
-            A <= D
+            A <= D;
         end
     end
 
     always_comb begin
     // multiply A and B registers
-        out = A * B
-        Dout = A0 ? B : A // if A0 high, then B, else A (basically muxing using A0)
+        out = A * B;
+        Dout = A0 ? B : A; // if A0 high, then B, else A (basically muxing using A0)
     end
 
     // output at the end if CE high and RWB low
-    assign D = (CE && !RWB) ? Dout : 8'hZZ
+    assign D = (CE && !RWB) ? Dout : 8'hZZ;
+    
 endmodule:
---
+++ multaccel_tb.sv
@@ -0,0 +1,20 @@
+// testbench for 6502 multiplication accelerator
+
+module tb;
+    timeunit 1ns;
+    timeprecision 1ps;
+
+    logic clk;
+    logic RWB;
+    logic CE;
+    logic A0;
+    logic [7:0] A;
+    logic [7:0] B;
+    logic [7:0] Dout;
+    logic [15:0] out;
+    wire [7:0] D;
+    
+
+
+
+endmodule
